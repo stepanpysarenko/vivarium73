9422b7e6d29ffe316aea9742ac044650a1a6bdc3
Stepan Pysarenko <stepan.pysarenko@gmail.com>
2025-04-05 01:04:36 +0300

    fix

--- server/game.js
+++ server/game.js
@@ -110,7 +110,7 @@ function updateStats() {
 
 function updateFood() {
     var totalCreatureEnergy = state.creatures.reduce((total, creature) => total + creature.energy, 0);
-    while ((totalCreatureEnergy + state.food.length * FOOD_ENERGY) < TOTAL_ENERGY) && state.food.length < FOOD_MAX_COUNT) {
+    while (((totalCreatureEnergy + state.food.length * FOOD_ENERGY) < TOTAL_ENERGY) && state.food.length < FOOD_MAX_COUNT) {
         state.food.push(initFood());
     }
 }
