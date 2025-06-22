acf3262239a3b3fdd61cf3d90ca0c23ffbe12811
Stepan Pysarenko <stepan.pysarenko@gmail.com>
2025-06-22 23:10:49 +0300

    Fix boundary check and websocket import

--- server/src/actions.js
+++ server/src/actions.js
@@ -2,7 +2,7 @@ const CONFIG = require("./config");
 const { addFood, getFoodCount } = require("./state");
 
 function placeFood(x, y) {
-    const coordinatesValid = typeof x === "number" && typeof y == "number" 
+    const coordinatesValid = typeof x === "number" && typeof y === "number"
         && x >= 0 && x < CONFIG.GRID_SIZE && y >= 0 && y < CONFIG.GRID_SIZE;
     if (!coordinatesValid) {
         throw new Error("Invalid coordinates");
