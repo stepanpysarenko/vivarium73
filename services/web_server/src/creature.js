510b34f96d7997f60ab4db492535a72ce95f3140
Stepan Pysarenko <stepan.pysarenko@gmail.com>
2025-09-17 00:15:37 +0300

    change score formula

--- services/web_server/src/creature.js
+++ services/web_server/src/creature.js
@@ -44,7 +44,7 @@ async function initCreature(state, x = null, y = null, angle = 0.0, weights = nu
 }
 
 function getScore(creature) {
-    return Math.round(creature.stats.energyGained / Math.max(1, creature.stats.msLived) * 100000);
+    return Math.round(creature.stats.energyGained / 10);
 }
 
 function getNextCreatureId(state) {
